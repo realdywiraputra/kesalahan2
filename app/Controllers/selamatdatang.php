662711101a3b0f6b0f5780825e2964c978b05f44
realdywiraputra <realdywp@gmail.com>
2021-10-02 10:30:58 +0700

    menambahkan file lainnya

--- app/Controllers/selamatdatang.php
+++ app/Controllers/selamatdatang.php
@@ -1,20 +1,25 @@
 <?php
-namespace app\controllers;
+
+namespace App\Controllers;
 
 /**
  * created by phpstorm.
- * user: realdywiraputra
- * date: 24/09/2021
- * Time: 16.49
+ * user: agoenxz21
+ * date: 23/09/2021
+ * Time: 8:34
  */
-class selamatdatang extends basecontroller {
-
-    public function hal_awal(){
+class SelamatDatang extends BaseController
+{
+    public function hal_awal()
+    {
         return 'hello saya belajar CI4';
     }
-
-    public function beranda_login(){
-        return view( name: 'halaman/login');
+    public function beranda_login()
+    {
+        return view('halaman/login');
     }
-
+    public function daftar_member()
+    {
+        return view('halaman/daftar_member');
+    }
 }
